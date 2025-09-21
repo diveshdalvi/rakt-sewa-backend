564527224377e1ef948d0b2f6c2750a501a83287
Divesh Dalvi <dalvidivesh1@gmail.com>
2025-09-21 18:24:24 +0530

    Refactor getApprovedBloodRequests to filter by acceptedBy and improve formatting

--- src/controllers/public.controller.js
+++ src/controllers/public.controller.js
@@ -1,16 +1,24 @@
-const admin = require('../config/firebase-config');
+const admin = require("../config/firebase-config");
 const db = admin.firestore();
 
 const getApprovedBloodRequests = async (req, res) => {
-    try {
-        const requestsSnapshot = await db.collection('bloodRequests').where('status', '==', 'approved').get();
-        const requests = requestsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-        res.status(200).json(requests);
-    } catch (error) {
-        res.status(500).json({ message: 'Error fetching approved blood requests', error });
-    }
+  try {
+    const requestsSnapshot = await db
+      .collection("bloodRequests")
+      .where("acceptedBy", "!=", "null")
+      .get();
+    const requests = requestsSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    res.status(200).json(requests);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching approved blood requests", error });
+  }
 };
 
 module.exports = {
-    getApprovedBloodRequests,
+  getApprovedBloodRequests,
 };
